c71c20ca3ff2280a643d454982a2a7f8603eca10
harshvaghanii <harshvaghani98@gmail.com>
2024-08-09 11:59:21 -0400

    adding the repository layer and testing it using unit testing

--- src/test/java/com/example/codingshuttle/jpaTutorial/jpatutorial/JpatutorialApplicationTests.java
+++ src/test/java/com/example/codingshuttle/jpaTutorial/jpatutorial/JpatutorialApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.codingshuttle.jpaTutorial.jpatutorial;
 
+import com.example.codingshuttle.jpaTutorial.jpatutorial.entities.ProductEntity;
+import com.example.codingshuttle.jpaTutorial.jpatutorial.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 class JpatutorialApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ProductRepository repository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testRepository() {
+        ProductEntity entity = ProductEntity.builder()
+                .sku("acer123")
+                .title("Acer Laptop with i5 9th Gen Processor")
+                .price(BigDecimal.valueOf(1987.53))
+                .quantity(5)
+                .build();
+        ProductEntity savedEntity = repository.save(entity);
+        System.out.println(savedEntity);
+
+        assert (savedEntity.getSku().equals("acer123"));
+        assert (savedEntity.getTitle().equals("Acer Laptop with i5 9th Gen Processor"));
+        assert (savedEntity.getPrice().compareTo(BigDecimal.valueOf(1987.53))) == 0;
+        assert (savedEntity.getQuantity() == 5);
+    }
 
 }
