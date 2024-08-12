287b2826d4da02bc588c72f1e39e93b46023cf83
harshvaghanii <harshvaghani98@gmail.com>
2024-08-12 13:17:46 -0400

    implementing pagination and sorting logic

--- src/main/java/com/example/codingshuttle/jpaTutorial/jpatutorial/controllers/ProductController.java
+++ src/main/java/com/example/codingshuttle/jpaTutorial/jpatutorial/controllers/ProductController.java
@@ -2,6 +2,9 @@ package com.example.codingshuttle.jpaTutorial.jpatutorial.controllers;
 
 import com.example.codingshuttle.jpaTutorial.jpatutorial.entities.ProductEntity;
 import com.example.codingshuttle.jpaTutorial.jpatutorial.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/products")
@@ -17,13 +21,22 @@ public class ProductController {
 
     private final ProductRepository repository;
 
+    private final int PAGE_SIZE = 5;
+
     public ProductController(ProductRepository repository) {
         this.repository = repository;
     }
 
     @GetMapping
-    public List<ProductEntity> getAllProducts(@RequestParam(defaultValue = "id") String sortBy) {
-        return repository.findBy(Sort.by(Sort.Direction.DESC, sortBy, "price"));
+    public List<ProductEntity> getAllProducts(
+            @RequestParam(defaultValue = "") String title,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String order,
+            @RequestParam(defaultValue = "0") Integer pageNumber
+    ) {
+        Sort.Direction sortOrder = order.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Pageable pageable = PageRequest.of(pageNumber, PAGE_SIZE, Sort.by(sortOrder, sortBy, "price"));
+        return repository.findByTitleContainingIgnoreCase(title, pageable).getContent();
     }
 
 }
--- src/main/java/com/example/codingshuttle/jpaTutorial/jpatutorial/repositories/ProductRepository.java
+++ src/main/java/com/example/codingshuttle/jpaTutorial/jpatutorial/repositories/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.example.codingshuttle.jpaTutorial.jpatutorial.repositories;
 
 import com.example.codingshuttle.jpaTutorial.jpatutorial.entities.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,7 +24,7 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     List<ProductEntity> findByTitleLike(String title);
 
-    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
+    Page<ProductEntity> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     @Query("select e.title, e.price from ProductEntity e where e.title=:title and e.price=:price")
     Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
